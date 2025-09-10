408128999a2451809238091cf36cf66e57a2e94b
osooji <t25ystyc@icloud.com>
2025-09-11 02:59:34 +0900

    dodo

--- profill/src/components/header.tsx
+++ profill/src/components/header.tsx
@@ -8,9 +8,9 @@ const Header = () => {
     <div className='px-4 py-4 hover:text-white hover:bg-gray-800 rounded-lg text-medium text-3xl'><Link href="/">ソードワールドRPG</Link>
     </div>
     <div className='flex gap-2 '>
-      <div className='px-4 py-2 border border-amber-400 bg-slate-100 hover:text-white hover:bg-gray-800 rounded-lg text-medium text-md'><Link href='/Users/signUp'>SignUp</Link></div>
+      {/* <div className='px-4 py-2 border border-amber-400 bg-slate-100 hover:text-white hover:bg-gray-800 rounded-lg text-medium text-md'><Link href='/Users/signUp'>SignUp</Link></div> */}
       {/* <div className='px-4 py-2 border border-amber-400 bg-slate-100 hover:text-white hover:bg-gray-800 rounded-lg text-medium text-md'><Link href='./UserIn/signin'>SignIn</Link></div> */}
-      <div className='px-4 py-2 border border-amber-400 bg-slate-100 hover:text-white hover:bg-gray-800 rounded-lg text-medium text-md'><Link href='/Users/signout'>アカウント削除</Link></div>
+      {/* <div className='px-4 py-2 border border-amber-400 bg-slate-100 hover:text-white hover:bg-gray-800 rounded-lg text-medium text-md'><Link href='/Users/signout'>アカウント削除</Link></div> */}
     </div>
   </div>
   )
